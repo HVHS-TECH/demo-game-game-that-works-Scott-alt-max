89099416287d250992d159f4b531f628fa58338f
Scott-alt-max <22069sb@hvhs.school.nz>
2025-02-23 23:30:50 +0000

    started demo game

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -1,1 +1,110 @@
-console.log("hajdigvo");
+/*******************************************************/
+// P5.play: t22_keyboard
+// Move sprite via keyboard
+// Written by Scott
+/*******************************************************/
+
+// setup()
+/*******************************************************/
+
+const GameWidth = 800;
+const GameHeight = 800;
+const NumberOfCoins = 200;
+const TextSize = 35;
+const GameSeconds = 5;
+var Score = 0;
+var GameRunning = true;
+var HaveTheyWon = false;
+
+function setup() {
+	console.log("setup: ");
+	cnv = new Canvas(GameWidth, GameWidth);
+	world.gravity.y = 10;
+
+	Player = new Sprite(500, 200, 100, 100, 'd');
+	Player.color = "White";
+	Player.vel.x = 2;
+	Player.bounciness = 0;
+
+	Spinner = new Sprite(500, 800, 100, 500, 'k');
+	Spinner.color = "Green";
+	Spinner.rotationSpeed = -5;
+	Spinner.vel.x = 0;
+	Spinner.bounciness = 0;
+	Spinner.friction = 200;
+
+	/*Ball = new Sprite(500, 300, 200, "d");
+	Ball.color = "Red";
+	Ball.vel.x = 0;
+	Ball.vel.y = 20;*/
+
+	WallGroup = new Group();
+	Wall_Left = new Sprite(GameWidth + 10, 0, 20, GameHeight * 2, 'k');
+	WallGroup.add(Wall_Left);
+	Wall_Right = new Sprite(-10, 0, 20, GameHeight * 2, 'k');
+	WallGroup.add(Wall_Right);
+	Wall_Top = new Sprite(0, -10, GameWidth * 2, 20, 'k');
+	WallGroup.add(Wall_Top);
+	Wall_Bottom = new Sprite(0, GameHeight + 10, GameWidth * 2, 20, 'k');
+	WallGroup.add(Wall_Bottom);
+
+	WallGroup.bounciness = 1.5;
+
+	CoinGroup = new Group();
+	for (var i = 0; i < NumberOfCoins; i++) {
+		MakeCoin();
+	}
+
+	function MakeCoin() {
+		Coin = new Sprite(Math.random() * GameWidth, Math.random() * GameHeight, 20, "d");
+		Coin.vel.x = 3;
+		Coin.vel.y = 4;
+		Coin.bounciness = 0.5;
+		Coin.friction = 0;
+		CoinGroup.add(Coin);
+	}
+
+	CoinGroup.collides(Player, CollisionFunction);
+	function CollisionFunction(WhichCoin, Spinner) {
+		WhichCoin.remove();
+        Score++;
+		//MakeCoin();
+	}
+}
+	
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+ 	background('red');
+	
+    Player.moveTowards(mouseX, mouseY, 0.05);
+	if (mouse.presses()) {
+		Player.moveTo(mouseX, mouseY, 10);
+	}
+
+    if(GameRunning == true) {
+        textSize(TextSize);
+        fill('white');
+        text("Score: " + Score + "/" + NumberOfCoins, 15, TextSize);
+        text("Timer: " + (GameSeconds - Math.floor(millis()/1000)), 15, TextSize * 2 + 15);
+    } else if (GameRunning == true && HaveTheyWon == false) {
+        text("Game Over :(", GameWidth / 2 - 110, GameHeight / 2 - 20);
+        text("Your Score: " + Score + "/" + NumberOfCoins, GameWidth / 2 - 150, GameHeight / 2 + TextSize)
+    }
+
+    if(millis() >= GameSeconds * 1000) {
+        EndGame();
+    }    
+    
+    function EndGame() {
+        GameRunning = false;
+        CoinGroup.remove();
+        Spinner.remove();
+        Player.remove();
+    }
+}
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
